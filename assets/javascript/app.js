cba306a81a01cf069b00d76bb9de932268a0706b
MaxH <MaxH@Maxs-MacBook-Pro.local>
2018-11-05 21:02:32 -0800

    timezone added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,29 @@
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?id=5392171&APPID=2a69d1f4ce8a367d123d72948f006681";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      });
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5392171&APPID=2a69d1f4ce8a367d123d72948f006681";
+
+$.ajax({
+  url: weatherURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+});
+
+var timezoneURL = "https://maps.googleapis.com/maps/api/timezone/json?location=37.3382,-121.8863&timestamp=1331161200&key=AIzaSyDA5pE6-JI4kSdYhzTn3qyAMzLYs_zITD8";
+
+$.ajax({
+  url: timezoneURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+
+  function calcTime(offset) {
+    var d = new Date();
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var nd = new Date(utc + (1000 * offset));
+
+    alert("The local time is " + nd.toLocaleString());
+  }
+
+  calcTime(-28800.0);
+});
+
+
